1ca0f8239a9afd46be101a02c898bb976d5265d2
Qu3sito Sad <vanegasdaliana@gmail.com>
2023-03-05 23:55:20 +0000

    actualizacion gather

--- gather.cpp
+++ gather.cpp
@@ -1,30 +1,21 @@
 #include <mpi.h>
 #include <stdio.h>
 
-#define ARRAY_SIZE 6
-
 int main(int argc, char** argv) {
-    int rank, size;
-    int sendbuf[ARRAY_SIZE];
-
     MPI_Init(&argc, &argv);
+
+    int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int recvbuf[ARRAY_SIZE*size];  // Array para recibir datos de todos los procesos
+    int sendbuf = rank;
+    int recvbuf[size];
 
-    // Inicializar el array de datos que se enviarán
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        sendbuf[i] = (rank + 1) * (i + 1);
-    }
+    MPI_Gather(&sendbuf, 1, MPI_INT, recvbuf, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Recoger los datos de todos los procesos en el proceso raíz (rank 0)
-    MPI_Gather(sendbuf, ARRAY_SIZE, MPI_INT, recvbuf, ARRAY_SIZE, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Imprimir los datos recibidos en el proceso raíz
     if (rank == 0) {
-        printf("Datos recibidos:\n");
-        for (int i = 0; i < ARRAY_SIZE*size; i++) {
+        printf("Process %d received data: ", rank);
+        for (int i = 0; i < size; i++) {
             printf("%d ", recvbuf[i]);
         }
         printf("\n");
@@ -33,3 +24,6 @@ int main(int argc, char** argv) {
     MPI_Finalize();
     return 0;
 }
+
+
+//salida 0,1,2,3
